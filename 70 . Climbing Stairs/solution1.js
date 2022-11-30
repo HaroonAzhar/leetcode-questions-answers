99db0e61ca5391cd8ad7092909063286139a4243
Haroon Azhar Khan <haroon1azhar@gmail.com>
2022-11-30 11:03:27 +0500

    added solution1 for problem 70

---
+++ 70 . Climbing Stairs/solution1.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  memo = {};
+  count = (k) => {
+    if (memo[k]) return memo[k];
+    if (k === 2) return 2;
+    if (k <= 1) return 1;
+    memo[k] = count(k - 1) + count(k - 2);
+    return memo[k];
+  };
+  return count(n);
+};
